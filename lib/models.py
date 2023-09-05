3dd9547e6cd8628c5e1a85e816ee6ec1c1be3fd1
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 02:37:34 +0300

    completed Lab Assignment

--- lib/models.py
+++ lib/models.py
@@ -1,21 +1,16 @@
-from sqlalchemy import func
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
-class Game(Base):
-    __tablename__ = 'games'
+class User(Base):
+    __tablename__ = 'users'
 
-    id = Column(Integer(), primary_key=True)
-    title = Column(String())
-    genre = Column(String())
-    platform = Column(String())
-    price = Column(Integer())
-    created_at = Column(DateTime(), server_default=func.now())
-    updated_at = Column(DateTime(), onupdate=func.now())
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    email = Column(String(100))
 
-    def __repr__(self):
-        return f'Game(id={self.id}, ' + \
-            f'title="{self.title}", ' + \
-            f'platform="{self.platform})"'
+# Replace 'your_database_uri' with your actual database URI
+engine = create_engine('your_database_uri')
+Session = sessionmaker(bind=engine)
